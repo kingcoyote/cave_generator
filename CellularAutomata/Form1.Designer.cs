5ec50d5114a8fcd5933027f8dfda4d03c37a07d2
Steve Phillips <kingcoyote85@gmail.com>
2015-02-14 10:52:11 -0800

    initial setup with my existing code, plus my Algorithms git repo, which i'll need to add some tools for connected components into

---
+++ CellularAutomata/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace CellularAutomata
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numSpeed = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.picOutput = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numSpeed)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Animation Speed";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.StartAutomata);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(9, 45);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Step Through";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.ContinueAutomata);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(463, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 262);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rules";
+            // 
+            // numSpeed
+            // 
+            this.numSpeed.Location = new System.Drawing.Point(115, 19);
+            this.numSpeed.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numSpeed.Name = "numSpeed";
+            this.numSpeed.Size = new System.Drawing.Size(79, 20);
+            this.numSpeed.TabIndex = 7;
+            this.numSpeed.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.numSpeed);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(463, 382);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 75);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Processing";
+            // 
+            // picOutput
+            // 
+            this.picOutput.Location = new System.Drawing.Point(12, 12);
+            this.picOutput.Name = "picOutput";
+            this.picOutput.Size = new System.Drawing.Size(445, 445);
+            this.picOutput.TabIndex = 10;
+            this.picOutput.TabStop = false;
+            this.picOutput.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawBox);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 469);
+            this.Controls.Add(this.picOutput);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Cellular Automata";
+            ((System.ComponentModel.ISupportInitialize)(this.numSpeed)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picOutput)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox picOutput;
+        private System.Windows.Forms.NumericUpDown numSpeed;
+    }
+}
+
---
+++ CellularAutomata/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**
+ * 1 - Generate all water grid
+ * 2 - Execute the rule sets in sequence
+ * 3 - Define connected components
+ * 4 - Identify largest contiguous component and mark it as the main region
+ * 5 - Attach any other components that are close enough
+ * 6 - Delete all remaining components
+ * 7 - Calculate water percentage
+ * 8 - Accept or reject final map
+ */
+
+namespace CellularAutomata
+{
+    public partial class Form1 : Form
+    {
+        private Color[,] _grid = new Color[128,128];
+        private Random _rng;
+        private Timer _timer;
+        private List<CaveRules> _rules;
+        private int _currentRule;
+        private int _currentPass;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            _rng = new Random((int)DateTime.Now.Ticks);
+            _timer = new Timer();
+            _timer.Enabled = false;
+            _timer.Tick += ContinueAutomata;
+            
+            _rules = new List<CaveRules>(2);
+            _rules.Add(new CaveRules { GridSize = 32, Iterations = 3, NeighborhoodSize = 1, NeighborhoodThreshold = 3, Initialization = 0.6f });
+            _rules.Add(new CaveRules { GridSize = 256, Iterations = 4, NeighborhoodSize = 2, NeighborhoodThreshold = 10, Initialization = 0.5f });
+        }
+
+        private void StartAutomata(object sender, EventArgs e)
+        {
+            StartAutomata();
+            _timer.Interval = 1000 / (int) numSpeed.Value;
+            _timer.Enabled = true;
+        }
+        
+        private void StartAutomata()
+        {
+            _grid = new Color[_rules[0].GridSize, _rules[0].GridSize];
+            _currentRule = 0;
+            _currentPass = 0;
+
+            for (var i = 0; i < _rules[0].GridSize; i++)
+            {
+                for (var j = 0; j < _rules[0].GridSize; j++)
+                {
+                    _grid[i, j] = Color.Aquamarine;
+                }
+            }
+
+            InitializeGrid(_rules[0].GridSize, _rules[0].Initialization);
+
+            picOutput.Refresh();
+        }
+
+        private void InitializeGrid(int gridSize, float threshold)
+        {
+            for (var i = 0; i < gridSize; i++)
+            {
+                for (var j = 0; j < gridSize; j++)
+                {
+                    if (_grid[i, j] == Color.SaddleBrown) continue;
+
+                    _grid[i, j] = _rng.NextDouble() < threshold ? Color.SaddleBrown : Color.Aquamarine;
+                }
+            }
+        }
+
+        private void ContinueAutomata(object sender, EventArgs e)
+        {
+            ContinueAutomata();
+        }
+
+        private void ContinueAutomata()
+        {
+            if (_currentRule == _rules.Count - 1 && _currentPass >= _rules[_currentRule].Iterations) return;
+            if (_currentPass == _rules[_currentRule].Iterations)
+            {
+                _currentRule++;
+                _currentPass = 0;
+                var newGrid = new Color[_rules[_currentRule].GridSize, _rules[_currentRule].GridSize];
+                var mul = (float)(_rules[_currentRule - 1].GridSize)/(float)(_rules[_currentRule].GridSize);
+                for(var i = 0; i < _rules[_currentRule].GridSize; i++)
+                {
+                    for (var j = 0; j < _rules[_currentRule].GridSize; j++)
+                    {
+                        newGrid[i, j] = _grid[(int) (i*mul), (int) (j*mul)];
+                    }
+                }
+                
+                _grid = newGrid;
+
+                InitializeGrid(_rules[_currentRule].GridSize, _rules[_currentRule].Initialization);
+            }
+
+            _grid = GenerateGrid(_grid, _rules[_currentRule].GridSize, _rules[_currentRule].NeighborhoodSize,
+                                 _rules[_currentRule].NeighborhoodThreshold);
+
+            _currentPass++;
+
+            if (_currentRule == _rules.Count - 1 && _currentPass >= _rules[_currentRule].Iterations)
+            {
+                DefineConnectedComponents();
+            };
+            
+            picOutput.Refresh();
+        }
+
+        private Color[,] GenerateGrid(Color[,] oldGrid, int gridSize, int size, float threshold)
+        {
+            var newGrid = new Color[gridSize, gridSize];
+            for (var i = 0; i < gridSize; i++)
+            {
+                for (var j = 0; j < gridSize; j++)
+                {
+                    var neighbors = 0;
+
+                    for (var k = -size; k <= size; k++)
+                    {
+                        for (var l = -size; l <= size; l++)
+                        {
+                            if (i + k < 0 || i + k >= gridSize || j + l < 0 || j + l >= gridSize || (k == 0 && l == 0)) continue;
+                            if (_grid[i + k, j + l] == Color.Aquamarine) neighbors++;
+                        }
+                    }
+
+                    newGrid[i, j] = neighbors > threshold ? Color.Aquamarine : Color.SaddleBrown;
+                }
+            }
+            return newGrid;
+        }
+
+        private void DefineConnectedComponents()
+        {
+            Console.WriteLine("defining connected components");
+        }
+
+        private void DrawBox(object sender, PaintEventArgs e)
+        {
+            var cellSize = (float)(picOutput.Width)/_rules[_currentRule].GridSize;
+            for (var i = 0; i < _rules[_currentRule].GridSize; i++)
+            {
+                for (var j = 0; j < _rules[_currentRule].GridSize; j++)
+                {
+                    var p = new SolidBrush(_grid[i, j]);
+                    e.Graphics.FillRectangle(p, i*cellSize, j*cellSize, cellSize, cellSize);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _timer.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StartAutomata();
+        }
+
+        private struct CaveRules
+        {
+            public int GridSize;
+            public int NeighborhoodSize;
+            public int NeighborhoodThreshold;
+            public int Iterations;
+            public float Initialization;
+        }
+    }
+}
